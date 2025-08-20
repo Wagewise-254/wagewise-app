c42329082bdd404f3e29394b71879c1c930441fb
ashley-chiteri <ochamiashley97@gmail.com>
2025-08-20 04:52:30 +0300

    fixed some bugs

--- src/components/dashboard/TopBar.tsx
+++ src/components/dashboard/TopBar.tsx
@@ -32,7 +32,7 @@ const TopBar: React.FC = () => {
         {/* Left Side: Logo */}
         <div className="flex items-center space-x-2">
           <Link to="/dashboard"> {/* Ensure this link points to your main dashboard */}
-            <img src="/icons/android-chrome-512x512.png" alt="Wagewise Logo" className="h-8 w-auto" />
+            <img src="/icons/android-chrome-512x512.png" alt="Wagewise" className="h-8 w-auto" />
           </Link>
         </div>
 
--- src/pages/onboarding/SplashScreen.tsx
+++ src/pages/onboarding/SplashScreen.tsx
@@ -47,7 +47,7 @@ const SplashScreen = () => {
             animate={{ opacity: 1 }}
             transition={{ delay: 0.5 }}
           >
-            Version 2.0.2
+            Version 2.0.3
           </motion.div>
         </motion.div>
       )}
