f78ca60b11e3145aa37d2d271bce20d4c0266470
ashley-chiteri <ochamiashley97@gmail.com>
2026-03-09 05:26:10 +0300

    Improved the payroll cycle

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { Toaster } from 'sonner';
-import AppRouterWrapper from './router';
-import { TooltipProvider } from "@/components/ui/tooltip"
 import './index.css'
-import { useEffect } from 'react';
-import { useAuthStore } from './stores/authStore';
-import OfflineBanner from "@/components/common/offlinebanner";
+import { Toaster } from 'sonner';
+import { TooltipProvider } from "@/components/ui/tooltip"
+import AppRouterWrapper from './router';
+//import OfflineBanner from "@/components/common/offlinebanner";
 
 function App() {
-  const { checkUser } = useAuthStore();
-
-  useEffect(() => {
-    checkUser();
-  }, [checkUser]);
 
   return (
     <>
-    <Toaster position="top-right" richColors />
-    <OfflineBanner />
-     <TooltipProvider><AppRouterWrapper /></TooltipProvider>
+    <Toaster position="top-center" richColors />
+    <TooltipProvider><AppRouterWrapper /></TooltipProvider>
+      
     </>
   )
 }
